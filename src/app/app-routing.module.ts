d1dbe033f7c3d192e7a92338e4195089e6bf9db3
ygttrl <62116544+ygttrl@users.noreply.github.com>
2021-11-08 23:18:52 +0300

    Section 10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RoleComponent } from './role/role.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"home",
+  component:HomeComponent,
+},
+{
+  path:"user",
+  component:UserComponent,
+},
+{
+  path:"user/:id",
+  component:UserDetailComponent,
+},
+{
+  path:"role",
+  component:RoleComponent,
+},
+{
+  path:"**", //Eğer aranan şekilde bir url yoksa(component) Home sayfsına(Home Component)
+  component:HomeComponent,
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from "ngx-spinner";
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { RoleComponent } from './role/role.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    RoleComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
